b751976267126e827065bb1215dce602bb183c06
Phillip Hamilton <xphilup@hotmail.com>
2017-12-11 11:52:06 -0800

    To make Jupyter work?

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = ''
+SITEURL = 'https://github.com/xphilup2'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
